3b49984ad5446dbbd3f416e9ef2fdceded767a33
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2022-04-09 23:16:39 +0430

    passed FormulizedMeasurmentUnit_ShouldReturnSameValueForSameIMeasurementUnit

---
+++ MUManagementSystem.Domain.UnitTests/Extensions/IMeasurementUnitExtTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using MUManagementSystem.Domain.Models.Abstractions;
+using MUManagementSystem.Domain.Models;
+using Xunit;
+using MUManagementSystem.Domain.Extensions;
+using Shouldly;
+
+namespace MUManagementSystem.Domain.UnitTests.Extensions
+{
+    public class IMeasurementUnitExtTests
+    {
+        [Fact]
+        public void FormulizedMeasurmentUnit_ShouldReturnSameValueForSameIMeasurementUnit()
+        {
+            // Given
+            IMeasurementUnit measurementUnit = new FormulizedMeasurmentUnit(
+                Guid.NewGuid() , 
+                Guid.NewGuid(), 
+                "arbitrary", 
+                "arb",
+                new MeasurementUnitFormula("a + 15") ,
+                new MeasurementUnitFormula("a - 15"));
+
+            decimal givenValue = 12.3M;
+
+            // When
+            decimal result = measurementUnit.ConvertTo(givenValue, measurementUnit);
+
+            // Then
+            result.ShouldBe(givenValue);
+        }
+
+        [Fact]
+        public void PrimaryMeasurementUnit_ShouldReturnSameValueForSameIMeasurementUnit()
+        {
+            // Given
+            IMeasurementUnit measurementUnit = new PrimaryMeasurementUnit(
+                Guid.NewGuid(), "arbitrary", "arb", "some dimension");
+
+            decimal givenValue = 12.3M;
+
+            // When
+            decimal result = measurementUnit.ConvertTo(givenValue, measurementUnit);
+
+            // Then
+            result.ShouldBe(givenValue);
+        }
+    }
+}

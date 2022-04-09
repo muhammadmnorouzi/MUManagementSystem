f104908b808a185e0e7bb19f272c0fd5b703953a
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2022-04-09 17:13:39 +0430

    passed ShouldThrowIfStringIsNullOrEmpty

---
+++ MUManagementSystem.Common.UnitTests/Extensions/GuardExtTests.cs
@@ -0,0 +1,25 @@
+﻿using MUManagementSystem.Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MUManagementSystem.Common.UnitTests.Extensions
+{
+    public class GuardExtTests
+    {
+        [Fact]
+        public void ShouldThrowIfStringIsNullOrEmpty()
+        {
+            // Given
+            string emptyString = string.Empty;
+            string nullString = null!;
+
+            // Then
+            Assert.Throws<ArgumentNullException>(() => emptyString.ThrowIfNullOrEmpty());
+            Assert.Throws<ArgumentNullException>(() => nullString.ThrowIfNullOrEmpty());
+        }
+    }
+}

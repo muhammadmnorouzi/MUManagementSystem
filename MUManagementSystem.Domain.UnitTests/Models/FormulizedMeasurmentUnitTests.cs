dc2f2799a1857314bcb940dddb4bff2165c749c3
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2022-04-09 22:49:00 +0430

    passed Ctor_ShouldThrowExceptionsOnBadArguments

--- MUManagementSystem.Domain.UnitTests/Models/FormulizedMeasurmentUnitTests.cs
+++ MUManagementSystem.Domain.UnitTests/Models/FormulizedMeasurmentUnitTests.cs
@@ -17,13 +17,25 @@ namespace MUManagementSystem.Domain.UnitTests.Models
             Guid id = Guid.NewGuid();
             Guid baseMeasurementUnitId = Guid.NewGuid();
             string goodStr = "some thing";
+            MeasurementUnitFormula goodFormula = new MeasurementUnitFormula("a");
 
-            //Should.Throw<ArgumentNullException>(() => new FormulizedMeasurmentUnit(id , baseMeasurementUnitId ,
-            //    Guid.NewGuid() , 
-            //     Guid.NewGuid() ,
-            //    null!,
-            //    "some thing",
-            //    )
+            Should.Throw<ArgumentNullException>(() =>
+                new FormulizedMeasurmentUnit(id, baseMeasurementUnitId, null!, goodStr, goodFormula, goodFormula));
+
+            Should.Throw<ArgumentNullException>(() =>
+                new FormulizedMeasurmentUnit(id, baseMeasurementUnitId, string.Empty, goodStr, goodFormula, goodFormula));
+
+            Should.Throw<ArgumentNullException>(() =>
+                new FormulizedMeasurmentUnit(id, baseMeasurementUnitId, goodStr, null!, goodFormula, goodFormula));
+
+            Should.Throw<ArgumentNullException>(() =>
+                new FormulizedMeasurmentUnit(id, baseMeasurementUnitId, goodStr, string.Empty, goodFormula, goodFormula));
+
+            Should.Throw<ArgumentNullException>(() =>
+                new FormulizedMeasurmentUnit(id, baseMeasurementUnitId, goodStr, goodStr, null!, goodFormula));
+
+            Should.Throw<ArgumentNullException>(() =>
+                new FormulizedMeasurmentUnit(id, baseMeasurementUnitId, goodStr, goodStr, goodFormula, null!));
         }
     }
 }

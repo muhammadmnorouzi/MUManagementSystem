ea65989fdafc778200a288a15fe8268ba82f2845
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2022-04-10 11:24:19 +0430

    passed GetBaseMeasurementUnitId_ShouldReturnBaseMeasurementUnitId for PrimaryMeasurementUnit

---
+++ MUManagementSystem.Domain.UnitTests/Models/PrimaryMeasurementUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using MUManagementSystem.Domain.Models;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MUManagementSystem.Domain.UnitTests.Models
+{
+    public class PrimaryMeasurementUnitTests:TestsBase
+    {
+        [Fact]
+        public void GetBaseMeasurementUnitId_ShouldReturnBaseMeasurementUnitId()
+        {
+            PrimaryMeasurementUnit measurementUnit = CreatePrimaryMeasurementUnit();
+
+            measurementUnit.GetBaseMeasurementUnitId().ShouldBe(measurementUnit.Id);
+        }
+    }
+}
